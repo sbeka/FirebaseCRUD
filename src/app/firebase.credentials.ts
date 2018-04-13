6257ceaf55e10522e49c048335aa4b1f2183678e
Bekzhan Seitkali <sbeka1996@gmail.com>
2018-04-13 19:08:23 +0600

    first

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+    apiKey: "AIzaSyDdQjqQRzL4sRfQmcBEk6GI0AtZzUf5y38",
+    authDomain: "fir-crud-7507c.firebaseapp.com",
+    databaseURL: "https://fir-crud-7507c.firebaseio.com",
+    projectId: "fir-crud-7507c",
+    storageBucket: "fir-crud-7507c.appspot.com",
+    messagingSenderId: "499108341207"
+}
---
+++ src/pages/add-shopping/add-shopping.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+
+@Component({
+  selector: 'page-add-shopping',
+  templateUrl: 'add-shopping.html',
+})
+export class AddShoppingPage {
+
+  shoppingItem = {} as ShoppingItem;
+
+  shoppingItemRef$: FirebaseListObservable<ShoppingItem[]>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private database: AngularFireDatabase) {
+    this.shoppingItemRef$ = this.database.list('shopping-list');
+  }
+
+  addShoppingItem(shoppingItem: ShoppingItem) {
+    this.shoppingItemRef$.push({
+      itemName: this.shoppingItem.itemName,
+      itemNumber: Number(this.shoppingItem.itemNumber)
+    });
+    this.shoppingItem = {} as ShoppingItem;
+    this.navCtrl.pop();
+  }
+
+}
